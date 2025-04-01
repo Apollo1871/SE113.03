c2bb180f7f47c16b8f9a5266b7168cb41ca3ef85
Ben Ali <bjali@email.neit.edu>
2025-04-01 10:55:36 -0400

    added main page

---
+++ ClassSite/week2/js/script.js
@@ -0,0 +1,5 @@
+
+                var message = prompt("what is your name?");
+                //alert(message + "Woulndn't you like to be a pepper too??");
+                document.getElementById("Welcome-message").innerHTML = `Welcome ${message} to week 2 lab.`;
+                document.getElementById("footer").style.color = "red";
